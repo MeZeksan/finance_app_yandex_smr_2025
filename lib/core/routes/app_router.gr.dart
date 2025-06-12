bf7a27a8975b7f4e00cd37d09d61e6bba0376d9b
MeZeksan <bulzak74848@gmail.com>
2025-06-12 17:46:10 +0500

    Навигационный бар

--- lib/core/routes/app_router.gr.dart
+++ lib/core/routes/app_router.gr.dart
@@ -7,3 +7,101 @@
 
 // ignore_for_file: type=lint
 // coverage:ignore-file
+
+part of 'app_router.dart';
+
+/// generated route for
+/// [AccountScreen]
+class AccountRoute extends PageRouteInfo<void> {
+  const AccountRoute({List<PageRouteInfo>? children})
+      : super(AccountRoute.name, initialChildren: children);
+
+  static const String name = 'AccountRoute';
+
+  static PageInfo page = PageInfo(
+    name,
+    builder: (data) {
+      return const AccountScreen();
+    },
+  );
+}
+
+/// generated route for
+/// [ArticlesScreen]
+class ArticlesRoute extends PageRouteInfo<void> {
+  const ArticlesRoute({List<PageRouteInfo>? children})
+      : super(ArticlesRoute.name, initialChildren: children);
+
+  static const String name = 'ArticlesRoute';
+
+  static PageInfo page = PageInfo(
+    name,
+    builder: (data) {
+      return const ArticlesScreen();
+    },
+  );
+}
+
+/// generated route for
+/// [ExpensesScreen]
+class ExpensesRoute extends PageRouteInfo<void> {
+  const ExpensesRoute({List<PageRouteInfo>? children})
+      : super(ExpensesRoute.name, initialChildren: children);
+
+  static const String name = 'ExpensesRoute';
+
+  static PageInfo page = PageInfo(
+    name,
+    builder: (data) {
+      return const ExpensesScreen();
+    },
+  );
+}
+
+/// generated route for
+/// [IncomesScreen]
+class IncomesRoute extends PageRouteInfo<void> {
+  const IncomesRoute({List<PageRouteInfo>? children})
+      : super(IncomesRoute.name, initialChildren: children);
+
+  static const String name = 'IncomesRoute';
+
+  static PageInfo page = PageInfo(
+    name,
+    builder: (data) {
+      return const IncomesScreen();
+    },
+  );
+}
+
+/// generated route for
+/// [MainWrapperPage]
+class MainWrapperRoute extends PageRouteInfo<void> {
+  const MainWrapperRoute({List<PageRouteInfo>? children})
+      : super(MainWrapperRoute.name, initialChildren: children);
+
+  static const String name = 'MainWrapperRoute';
+
+  static PageInfo page = PageInfo(
+    name,
+    builder: (data) {
+      return const MainWrapperPage();
+    },
+  );
+}
+
+/// generated route for
+/// [SettingsScreen]
+class SettingsRoute extends PageRouteInfo<void> {
+  const SettingsRoute({List<PageRouteInfo>? children})
+      : super(SettingsRoute.name, initialChildren: children);
+
+  static const String name = 'SettingsRoute';
+
+  static PageInfo page = PageInfo(
+    name,
+    builder: (data) {
+      return const SettingsScreen();
+    },
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,16 @@
+import 'package:finance_app_yandex_smr_2025/core/routes/app_router.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
 
+  final _appRouter = AppRouter();
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: const Center(
-          child: Text('Привет'),
-        ),
-      ),
-    );
+    return MaterialApp.router(routerConfig: _appRouter.config());
   }
 }
